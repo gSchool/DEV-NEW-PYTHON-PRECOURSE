d1aac0ff8f167940009f2897e4925b34c0c2a7f0
seanreed1111 <seanreed1111@gmail.com>
2020-02-03 09:59:37 -0500

    adds regex to convert from camel case to snake case

--- file_processing/convert_to_snake_case.py
+++ file_processing/convert_to_snake_case.py
@@ -3,11 +3,20 @@ import re
 
 source_path = os.path.join('..','testdir')
 target_path = os.path.join('..','snake_case_challenges')
+titles = re.compile(r'title:\s+([a-zA-Z0-9]*)')
+camel_case = re.compile(r"(.+?)([A-Z])")
+def snake_case(match):
+    return match.group(1).lower() + "_" + match.group(2).lower()
 
 for file in os.listdir(source_path):
   filepath = os.path.join(source_path, file)
   with open(filepath, "rt") as f:
-    data = f.read()
-    titles = re.compile(r'title:\s+([a-zA-Z]*)')
-    function_names = titles.findall(data)
-    print(file,function_names)
+    markdown = f.read()
+    old_function_names = titles.findall(markdown)
+    new_function_names = [re.sub(camel_case, snake_case, name, 0) for name in old_function_names]
+    print(file, new_function_names)
+
+# split function names according to upper case letters in function name ONLY!
+# if not in camel case, ignore function name
+# lowercase all words
+# replace original function name with lowercase snake function names
